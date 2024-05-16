e2f1fcfc411cab55ad9726b0395210f0c52c3dd6
Hana <a.hana@aui.ma>
2024-05-16 21:18:49 +0100

    feat: get params

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
-extern crate clap;
-use clap::{App, Arg};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// The link to the file to download
+    #[clap(short, long)]
+    link: String,
+
+    /// The output file. If not specified, the file will be saved in the current directory
+    #[clap(short, long, default_value = ".")]
+    output: String,
+
+    /// The number of threads to use for downloading
+    #[clap(short = 'p', long, default_value = "4")]
+    threads: u32,
+
+    /// The number of retries to use for downloading
+    #[clap(short, long, default_value = "3")]
+    retries: u32,
+
+    /// The timeout for each request
+    #[clap(short, long, default_value = "10")]
+    timeout: u32,
+}
 
 fn main() {
-    // println!("Hello, world!");
-    let matches = App::new("MyApp")
-        .arg(
-            Arg::with_name("input")
-                .short("i")
-                .long("input")
-                .value_name("FILE")
-                .help("Input file")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("output")
-                .short("o")
-                .long("output")
-                .value_name("FILE")
-                .help("Output file")
-                .takes_value(true),
-        )
-        .get_matches();
+    let args = Args::parse();
+    println!("{:?}", args);
 }
