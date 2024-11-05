b28fe87df6eb66ff96f064ab04a2f6f155d0dde2
Hana <a.hana@aui.ma>
2024-11-05 15:37:06 +0100

    feat: add unit tests

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,9 @@
+use clap::Parser;
+use reqwest::blocking::Client;
 use std::cmp::min;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::sync::{Arc, Mutex};
-use clap::Parser;
-use reqwest::blocking::Client;
 use validator::Validate;
 
 #[derive(Parser, Validate, Debug, Clone)]
@@ -20,12 +20,20 @@ pub struct Args {
 
     /// The number of threads to use for downloading.
     #[clap(short = 'p', long, default_value = "4")]
-    #[validate(range(min = 1, max = 32, code = "The number of threads must be between 1 and 32"))]
+    #[validate(range(
+        min = 1,
+        max = 32,
+        code = "The number of threads must be between 1 and 32"
+    ))]
     pub(crate) threads: u32,
 
     /// The number of retries to use for downloading.
     #[clap(short, long, default_value = "3")]
-    #[validate(range(min = 1, max = 10, code = "The number of retries must be between 1 and 10"))]
+    #[validate(range(
+        min = 1,
+        max = 10,
+        code = "The number of retries must be between 1 and 10"
+    ))]
     pub(crate) retries: u32,
 
     /// The timeout for each request.
@@ -34,8 +42,18 @@ pub struct Args {
     pub(crate) timeout: u32,
 }
 
-
-pub(crate) fn downloader_thread(url: &str, output: &str, retries: u32, timeout: u32, start: u64, end: u64, index: u32, file_name: &str, total: u32, progress: Arc<Mutex<f64>>) -> Result<(), String> {
+pub(crate) fn downloader_thread(
+    url: &str,
+    output: &str,
+    retries: u32,
+    timeout: u32,
+    start: u64,
+    end: u64,
+    index: u32,
+    file_name: &str,
+    total: u32,
+    progress: Arc<Mutex<f64>>,
+) -> Result<(), String> {
     // Create a new reqwest client
     let client = Client::builder()
         .timeout(std::time::Duration::from_secs(timeout as u64))
@@ -62,31 +80,123 @@ pub(crate) fn downloader_thread(url: &str, output: &str, retries: u32, timeout: 
         }
     };
     while current_bytes < end && try_count < retries {
-        match client.get(url)
-            .header("Range", format!("bytes={}-{}", current_bytes, min(current_bytes + buffer_size as u64, end)))
-            .send() {
-            Ok(response) => {
-                match response.bytes() {
-                    Ok(bytes) => {
-                        current_bytes += bytes.len() as u64;
-                        downloaded_bytes += bytes.len() as u64;
-                        f.write_all(&bytes).unwrap();
-                        let mut progress_val = progress.lock().unwrap();
-                        *progress_val += bytes.len() as f64 / ((end - start) * total as u64) as f64;
-                        println!("Downloaded {:.2}% of the file", *progress_val * 100.0);
-                    }
-                    Err(e) => {
-                        println!("[downloader_thread] Error: {}. Retrying for the {}th time.", e, try_count + 1);
-                        try_count += 1;
-                    }
+        match client
+            .get(url)
+            .header(
+                "Range",
+                format!(
+                    "bytes={}-{}",
+                    current_bytes,
+                    min(current_bytes + buffer_size as u64, end)
+                ),
+            )
+            .send()
+        {
+            Ok(response) => match response.bytes() {
+                Ok(bytes) => {
+                    current_bytes += bytes.len() as u64;
+                    downloaded_bytes += bytes.len() as u64;
+                    f.write_all(&bytes).unwrap();
+                    let mut progress_val = progress.lock().unwrap();
+                    *progress_val += bytes.len() as f64 / ((end - start) * total as u64) as f64;
+                    println!("Downloaded {:.2}% of the file", *progress_val * 100.0);
+                }
+                Err(e) => {
+                    println!(
+                        "[downloader_thread] Error: {}. Retrying for the {}th time.",
+                        e,
+                        try_count + 1
+                    );
+                    try_count += 1;
                 }
             },
             Err(e) => {
-                println!("[downloader_thread] Error: {}. Retrying for the {}th time.", e, try_count + 1);
+                println!(
+                    "[downloader_thread] Error: {}. Retrying for the {}th time.",
+                    e,
+                    try_count + 1
+                );
                 try_count += 1;
             }
         }
     }
-    println!("Thread {} downloaded {} bytes, expected {} bytes", index, downloaded_bytes, end - start);
+    println!(
+        "Thread {} downloaded {} bytes, expected {} bytes",
+        index,
+        downloaded_bytes,
+        end - start
+    );
+    if try_count == retries {
+        return Err("Error: download failed".to_string());
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_downloader_thread() {
+        let url = "https://i.imgur.com/z4d4kWk.jpg";
+        let output = ".";
+        let retries = 3;
+        let timeout = 10;
+        let start = 0;
+        let end = 3000;
+        let index = 0;
+        let file_name = "100MB.bin";
+        let total = 2;
+        let progress = Arc::new(Mutex::new(0.0));
+        assert_eq!(
+            downloader_thread(
+                url, output, retries, timeout, start, end, index, file_name, total, progress
+            ),
+            Ok(())
+        );
+    }
+
+    #[test]
+    fn test_downloader_thread_invalid_url() {
+        let url = "https://speed.hetzner.de/100MB";
+        let output = "/tmp/";
+        let retries = 3;
+        let timeout = 10;
+        let start = 0;
+        let end = 1024 * 1024 * 10;
+        let index = 0;
+        let file_name = "100MB.bin";
+        let total = 4;
+        let progress = Arc::new(Mutex::new(0.0));
+        assert_eq!(
+            downloader_thread(
+                url, output, retries, timeout, start, end, index, file_name, total, progress
+            ),
+            Err("Error: download failed".to_string())
+        );
+    }
+
+    #[test]
+    fn args_validation() {
+        let args = Args {
+            link: "https://example.com".to_string(),
+            output: "/tmp/".to_string(),
+            threads: 4,
+            retries: 3,
+            timeout: 10,
+        };
+        assert_eq!(args.validate().is_ok(), true);
+    }
+
+    #[test]
+    fn args_validation_invalid_url() {
+        let args = Args {
+            link: "example.comodo".to_string(),
+            output: "/tmp/".to_string(),
+            threads: 4,
+            retries: 3,
+            timeout: 10,
+        };
+        assert_eq!(args.validate().is_err(), true);
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
+use reqwest::blocking::Client;
 use std::fs::File;
 use std::io::{Read, Write};
-use reqwest::blocking::Client;
 
 pub(crate) fn validate_url(url: &str) -> Result<(), String> {
     if url.is_empty() {
@@ -17,41 +17,78 @@ pub(crate) fn validate_url(url: &str) -> Result<(), String> {
 pub fn check_server_compatibility(link: &str) -> Result<(u64, String), String> {
     let client = Client::new();
 
-    match client.head(link)
-        .send() {
+    match client.head(link).send() {
         Ok(response) => {
             let file_name = response.url().path().split("/").last().unwrap().to_string();
             match response.headers().get("Content-Length") {
                 Some(value) => {
                     let content_length = value.to_str().unwrap().parse().unwrap();
                     match response.headers().get("Accept-Ranges") {
-                        Some(_) => {
-                            Ok((content_length, file_name))
-                        }
-                        None => {
-                            Err("The server does not support range requests".to_string())
-                        }
+                        Some(_) => Ok((content_length, file_name)),
+                        None => Err("The server does not support range requests".to_string()),
                     }
                 }
-                None => {
-                    Err("The server did not return the content length".to_string())
-                }
+                None => Err("The server did not return the content length".to_string()),
             }
-        },
-        Err(e) => {
-            Err(format!("Error: {}", e))
         }
+        Err(e) => Err(format!("Error: {}", e)),
     }
 }
 
 pub(crate) fn merge_files(output: String, file_name: String, threads: u32) {
     let mut output_file = File::create(format!("{}/{}", output, file_name)).unwrap();
     for index in 0..threads {
-        let mut f = File::open(format!("{}/{}.part{}_{}", output, file_name, index, threads)).unwrap();
+        let mut f = File::open(format!(
+            "{}/{}.part{}_{}",
+            output, file_name, index, threads
+        ))
+        .unwrap();
         let mut buffer = vec![];
         f.read_to_end(&mut buffer).unwrap();
         output_file.write_all(&buffer).unwrap();
-        std::fs::remove_file(format!("{}/{}.part{}_{}", output, file_name, index, threads)).unwrap();
+        std::fs::remove_file(format!(
+            "{}/{}.part{}_{}",
+            output, file_name, index, threads
+        ))
+        .unwrap();
     }
     println!("Done!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn url_validation() {
+        assert_eq!(validate_url("https://example.com").is_ok(), true);
+        assert_eq!(validate_url("http://example.com").is_ok(), true);
+        assert_eq!(validate_url("example.com").is_err(), true);
+        assert_eq!(validate_url("").is_err(), true);
+    }
+
+    #[test]
+    fn server_compatibility() {
+        let (content_length, file_name) =
+            check_server_compatibility("https://i.imgur.com/z4d4kWk.jpg").unwrap();
+        assert_eq!(content_length > 0, true);
+        assert_eq!(file_name.is_empty(), false);
+    }
+
+    #[test]
+    fn merge_files_test() {
+        let output = "tests".to_string();
+        let file_name = "test.txt".to_string();
+        let threads = 2;
+        let mut f1 = File::create(format!("{}/{}.part0_{}", output, file_name, threads)).unwrap();
+        f1.write_all(b"Hello").unwrap();
+        let mut f2 = File::create(format!("{}/{}.part1_{}", output, file_name, threads)).unwrap();
+        f2.write_all(b"World").unwrap();
+        merge_files(output.clone(), file_name.clone(), threads);
+        let mut f = File::open(format!("{}/{}", output, file_name)).unwrap();
+        let mut buffer = vec![];
+        f.read_to_end(&mut buffer).unwrap();
+        assert_eq!(buffer, b"HelloWorld");
+        std::fs::remove_file(format!("{}/{}", output, file_name)).unwrap();
+    }
 }
